acf2eec6a4e7bcd8968e8ed99d6736313df31f7b
Romy <701224@student.inholland.nl>
2026-04-26 21:06:01 +0200

    localstorage api toegevoegd!

--- src/api/spotifyApi.js
+++ src/api/spotifyApi.js
@@ -2,7 +2,7 @@ import songs from '../data/dummySongs.json';
 
 let cachedToken = null;
 let tokenExpiry = 0;
-export let currentSongs = undefined;//songs;
+export let currentSongs = songs;
 
 async function fetchSpotifyAuthToken() {
     const client_id = import.meta.env.API_KEY_SPOTIFY_CLIENT_ID;
