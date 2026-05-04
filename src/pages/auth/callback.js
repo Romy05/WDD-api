ff647441c94a483d8c6969a1a1dd598254b650fa
Romy <701224@student.inholland.nl>
2026-05-04 14:21:52 +0200

    werkende callback url

--- src/pages/auth/callback.js
+++ src/pages/auth/callback.js
@@ -3,10 +3,9 @@
 
 export async function GET({ url, cookies, redirect }) {
     const code = url.searchParams.get('code');
-    const base_url = import.meta.env.BASE_URL;
     const client_id = import.meta.env.API_KEY_SPOTIFY_CLIENT_ID;
     const client_secret = import.meta.env.API_KEY_SPOTIFY_CLIENT_SECRET;
-    const redirect_uri = `${base_url}/auth/callback`;
+    const redirect_uri = `https://wdd-api.onrender.com//auth/callback`;
 
     const AuthHeader = btoa(client_id + ':' + client_secret);
 
